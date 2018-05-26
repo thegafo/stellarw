7390e253b70684069f7a9a98eccf17caed721834
gafo <gafopi@gmail.com>
2018-05-26 09:11:03 -0400

    package updates

---
+++ examples/listen.js
@@ -0,0 +1,19 @@
+
+const Wallet = require('../index');
+
+(async () => {
+  var wallet = new Wallet(null, true);
+  wallet.keys = await wallet.generateKeys();
+  await wallet.createAccount(wallet.keys.publicKey());
+
+  console.log('created wallet');
+  console.log(wallet.keys.publicKey(), wallet.keys.secret());
+  console.log();
+
+  wallet.on('tx', tx => {
+    console.log(JSON.stringify(tx, null, 2));
+  })
+
+  wallet.listen();
+
+})();
